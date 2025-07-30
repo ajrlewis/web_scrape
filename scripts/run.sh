8051b810561840ad2113b275c9bd4bc482943d52
ajrlewis <hello@ajrlewis.com>
2025-07-30 09:46:25 +0100

    Taking URL argument in bash script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Run with: ./scripts/run.sh
-
 source .venv/bin/activate
-# PYTHONPATH=src python3 -m web_scrape "solveintelligence.com/"
-PYTHONPATH=src python3 -m web_scrape "ajrlewis.com"
-# PYTHONPATH=src python3 -m web_scrape "https://uk.linkedin.com/company/downingllp"
-# PYTHONPATH=src python3 -m web_scrape "https://uk.linkedin.com/company/downingllp"
+PYTHONPATH=src python3 -m web_scrape "$1"
